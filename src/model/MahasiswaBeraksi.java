4a70ffb2ca0c3e272bc6515310a24e5961e6cb19
hasanurrahman <rahman.hasanur45@gmail.com>
2021-06-11 17:48:24 +0800

    memformat tanggal dan eksepsinya

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -1,16 +1,25 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        public static void main(String[] args) throws ParseException {
+
+            String polaTanggal = "dd-MM-yyyy";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630404";
         mahasiswa.nama = "Hasanur";
         mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("12-10-2000");
 
         System.out.println("Nama \t\t\t : " + mahasiswa.npm);
         System.out.println("Npm \t\t\t : " + mahasiswa.nama);
         System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+
     }
 }
