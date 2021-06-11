8e241ef65fbf57fb7b021f04fe946efff38e5e56
hasanurrahman <rahman.hasanur45@gmail.com>
2021-06-11 18:04:13 +0800

    getter and setter

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -5,21 +5,35 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
-        public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException {
 
-            String polaTanggal = "dd-MM-yyyy";
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
         Mahasiswa mahasiswa = new Mahasiswa();
-        mahasiswa.npm = "19630404";
-        mahasiswa.nama = "Hasanur";
-        mahasiswa.tanggalLahir = new Date();
-        mahasiswa.tanggalLahir = simpleDateFormat.parse("12-10-2000");
+        mahasiswa.setNpm("19630404");
+        mahasiswa.setNama("Hasanurrahman");
+        mahasiswa.setTanggalLahir(simpleDateFormat.parse("21-03-2000"));
 
-        System.out.println("Nama \t\t\t : " + mahasiswa.npm);
-        System.out.println("Npm \t\t\t : " + mahasiswa.nama);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+        System.out.println("Nama \t\t\t : " + mahasiswa.getNpm());
+        System.out.println("Npm \t\t\t : " + mahasiswa.getNama());
+        System.out.println("Tanggal Lahir \t : " + mahasiswa.getTanggalLahir());
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.setNpm("19638888");
+        mahasiswa2.setNama("Delapu");
+        mahasiswa2.setTanggalLahir(simpleDateFormat.parse("08-08-2008"));
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.getNpm());
+        System.out.println("Npm \t\t\t : " + mahasiswa2.getNama());
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.getTanggalLahir()));
+
+        mahasiswa.tampilkanAtribut();
+        mahasiswa.menyapa();
+        System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " tahun");
+
+        Mahasiswa m = new Mahasiswa("1963002","Dwi", new Date());
+        m.tampilkanAtribut();
 
     }
 }
